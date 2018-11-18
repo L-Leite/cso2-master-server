399856788801099b929fa7a3c96e776ba9c1141a
Ochii <ochii@leite.xyz>
2018-11-18 02:52:18 +0000

    Detect available network interface if no IP is passed as a cmd arg

---
+++ src/interfacepicker.ts
@@ -0,0 +1,106 @@
+import os from 'os'
+import readline from 'readline'
+
+export interface INetIntf {
+    name: string
+    net: os.NetworkInterfaceBase
+}
+
+/**
+ * builds a list of available network interfaces
+ * for the server to run with
+ */
+function getAvailableInterfaces(): INetIntf[] {
+    const results: INetIntf[] = []
+    const netInterfaces = os.networkInterfaces()
+
+    // the interfaces use string type keys
+    for (const intfName in netInterfaces) {
+        if (!netInterfaces.hasOwnProperty(intfName)) {
+            continue
+        }
+
+        for (const intf of netInterfaces[intfName]) {
+            // make sure it's an ipv4 interface and that it's not localhost or something
+            if (intf.family !== 'IPv4' || intf.internal !== false) {
+                continue
+            }
+
+            results.push({ name: intfName, net: intf })
+        }
+    }
+    return results
+}
+
+/**
+ * helper function that reads a number from the console's input
+ */
+function readChoiceFromConsole(): Promise<number> {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+
+    rl.prompt()
+
+    return new Promise<number>((resolve, reject) =>
+        rl.on('line', (input: any): void => {
+            const result: number = parseInt(input, 10)
+
+            rl.close()
+
+            if (isNaN(result)) {
+                reject('Choice is not a number')
+            } else {
+                resolve(result)
+            }
+        }))
+}
+
+/**
+ * queries the os for available network interfaces for the server to use
+ * if it only finds one interface, it returns it
+ * else it asks the user which one to use
+ */
+export async function getOrAskNetIntf(): Promise<INetIntf> {
+    const interfaces: INetIntf[] = getAvailableInterfaces()
+
+    if (interfaces.length === 0) {
+        console.error('Could not find any valid network interface!')
+        return null
+    }
+
+    let chosenIntf: INetIntf = null
+
+    if (interfaces.length !== 1) {
+    console.log('Please select an interface:')
+
+    let printIndex = 0
+
+    for (const intf of interfaces) {
+        console.log('(%i) - %s (%s)', printIndex++, intf.name, intf.net.address)
+    }
+
+    let choice: number = 0
+
+    try {
+        choice = await readChoiceFromConsole()
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+
+    if (choice < 0 || choice >= interfaces.length) {
+        console.error('The user did not enter a number, exiting...')
+        process.exit(1)
+    } else {
+        chosenIntf = interfaces[choice]
+    }
+    } else {
+        // if it finds only one interface, use it
+        chosenIntf = interfaces[0]
+    }
+
+    console.log('Using interface %s (%s)', chosenIntf.name, chosenIntf.net.address)
+    return chosenIntf
+}
--- src/server.ts
+++ src/server.ts
@@ -2,38 +2,50 @@
 
 'use strict'
 
-// add the src path to the module search path
+// add the src directory to the module search path
 import 'app-module-path/register'
 
 import program from 'commander'
 
+import { getOrAskNetIntf, INetIntf } from 'interfacepicker'
 import { ServerInstance } from 'serverinstance'
 
 program
   .version('0.2.0')
-  .option('-i, --ip-address <ip>', 'The IP address to be used by the server')
+  .option('-i, --ip-address [ip]', 'The IP address to be used by the server')
   .option('-p, --port-master [port]', 'The server\'s (TCP) port', 30001)
   .option('-P, --port-holepunch [port]', 'The server\'s holepunch (UDP) port', 30002)
   .parse(process.argv)
 
-// stop running if the user didn't pass the IP address
-if (program.ipAddress == null) {
-  console.error('No IP address given!')
-  program.outputHelp((str: string) => {
-    console.error(str)
-    return str
+// a little trick to allow us to use async functions
+async function startServer() {
+  let desiredIp: string = null
+
+  // prefer the ip address argument over asking the user for it
+  if (program.ipAddress) {
+    desiredIp = program.ipAddress
+  } else {
+    const intf: INetIntf = await getOrAskNetIntf()
+
+    // exit the program if the user failed to input an interface
+    if (intf == null) {
+      program.outputHelp((str: string) => {
+        console.error(str)
+        return str
+      })
+      process.exit(1)
+    }
+
+    desiredIp = intf.net.address
+  }
+
+  const masterServer: ServerInstance = new ServerInstance({
+    hostname: desiredIp,
+    portHolepunch: program.portHolepunch,
+    portMaster: program.portMaster,
   })
-  process.exit(1)
+
+  masterServer.listen()
 }
 
-console.log('Using command line arguments --ip-address as ' + program.ipAddress + '\n' +
-  '--port-master as ' + program.portMaster + '\n' +
-  '--port-holepunch as ' + program.portHolepunch)
-
-const masterServer: ServerInstance = new ServerInstance({
-  hostname: program.ipAddress,
-  portHolepunch: program.portHolepunch,
-  portMaster: program.portMaster,
-})
-
-masterServer.listen()
+startServer()
