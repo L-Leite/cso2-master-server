7e465abbd6351d3152a62e1acc87b5cc2be9e064
Ochii <ochii@leite.xyz>
2019-01-21 16:42:47 +0000

    Remove no longer needed hexy typescript index file

--- src/types/hexy.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'hexy' {
-    export function hexy(buffer: Buffer, config?: {
-        width?: number,
-        numbering?: string, format?: string, caps?: string, prefix?: string,
-        indent?: number, html?: boolean, offset?: number, length?: number,
-        display_offset?: number
-    }): string
-}
