8fa6c61d57ff53741c83faa4b2cbf98b26bfa087
Luís Leite <ochii@leite.xyz>
2019-04-07 01:06:12 +0100

    0.8.14

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import { getNetIntf, getOrAskNetIntf, INetIntf } from 'interfacepicker'
 import { ServerInstance } from 'serverinstance'
 
 program
-  .version('0.8.13')
+  .version('0.8.14')
   .option('-i, --ip-address [ip]', 'The IP address to be used by the server (don\'t use --interface with this)', null)
   .option('-I, --interface [intf]', 'The interface to be used by the server (don\'t use --ip-address with this)', null)
   .option('-p, --port-master [port]', 'The server\'s (TCP) port', 30001)
