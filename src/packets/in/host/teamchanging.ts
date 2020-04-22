cfb13cb47ee99fd757ddbec62c85813e0d2bd545
dounai2333 <45044404+dounai2333@users.noreply.github.com>
2020-04-23 03:28:10 +0800

    Create teamchanging.ts

---
+++ src/packets/in/host/teamchanging.ts
@@ -0,0 +1,26 @@
+import { InPacketBase } from 'packets/in/packet'
+
+/**
+ * requests an user's team change
+ * @class InHostTeamChanging
+ */
+ 
+export class InHostTeamChanging extends InPacketBase {
+    public type: number
+    public userId: number
+    public unk00: number
+    public unk01: number
+    public newTeam: number
+
+    /**
+    * parses the packet's data
+    */
+    protected parse(): void {
+        super.parse()
+        this.type = this.readUInt8()
+        this.userId = this.readUInt16()
+        this.unk00 = this.readUInt8()
+        this.unk01 = this.readUInt8()
+        this.newTeam = this.readUInt8()
+    }
+}
