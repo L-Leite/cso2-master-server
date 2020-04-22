59b7b28680508d7b6d0dcbe7d2ce5d01727c5686
dounai2333 <45044404+dounai2333@users.noreply.github.com>
2020-04-23 06:01:31 +0800

    Create itemusing.ts (IMPORTANT NOTE)
    for Leite: you can change it at any time to match the result you want

---
+++ src/packets/out/host/itemusing.ts
@@ -0,0 +1,14 @@
+import { OutPacketBase } from 'packets/out/packet'
+
+/**
+ * sends an user's item use
+ */
+
+export class OutHostItemUsing {
+    public static build(userId: number, itemId: number, outPacket: OutPacketBase): void {
+        outPacket.writeUInt32(userId)
+        outPacket.writeUInt32(itemId)
+        outPacket.writeUInt8(1) // how many left the item in your inventory.
+        // for Leite: you can change it at any time to match the result you want
+    }
+}
