8a0be355f509519d6a31667234e121e319d9469f
dounai2333 <45044404+dounai2333@users.noreply.github.com>
2020-04-26 21:34:11 +0800

    Update defaultmsg.ts

--- src/packets/out/chat/defaultmsg.ts
+++ src/packets/out/chat/defaultmsg.ts
@@ -7,13 +7,12 @@ import { PacketString } from 'packets/packetstring'
  * contains a message destinated to an user's current room
  */
 export class OutChatDefaultMsg {
-    public static build(sender: string, teamNum: number, message: string, outPacket: OutPacketBase): void {
-        const hasTeamNum: boolean = teamNum != null
+    public static build(sender: string, vipLevel: number, message: string, outPacket: OutPacketBase): void {
 
         outPacket.writeString(new PacketString(sender))
 
-        outPacket.writeUInt8(hasTeamNum === true ? 1 : 0)
-        outPacket.writeUInt8(hasTeamNum === true ? teamNum : 0)
+        outPacket.writeUInt8(vipLevel !== 0 ? 1 : 0)
+        outPacket.writeUInt8(vipLevel)
 
         outPacket.writeLongString(new PacketLongString(message))
     }
