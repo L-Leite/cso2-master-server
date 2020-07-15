2fb1e806cc0cd1a300a8f4bc40781b7dd27715f6
Luís Leite <luis@leite.xyz>
2020-07-15 19:25:35 +0100

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,13 @@
 'use strict'
 
-var cached = require('gulp-cached')
-var del = require('delete')
-var gulp = require('gulp')
-var mocha = require('gulp-mocha')
-var sourcemaps = require('gulp-sourcemaps')
-var ts = require('gulp-typescript')
-var tslint = require('gulp-tslint')
-var typedoc = require('gulp-typedoc')
-var util = require('gulp-util')
-
-gulp.task('clean:dist', () => {
-  util.log('Cleaning transpiled source...')
-  return del(['dist/**/*'])
-})
-
-gulp.task('clean:docs', () => {
-  util.log('Cleaning documentation...')
-  return del(['docs/**/*'])
-})
+const eslint = require('gulp-eslint')
+const gulp = require('gulp')
+const log = require('fancy-log')
+const ts = require('gulp-typescript')
+const typedoc = require('gulp-typedoc')
 
 gulp.task('readme', () => {
-  util.log('Generating documentation...')
+  log('Generating documentation...')
   return gulp.src(['src/**/*.ts'])
     .pipe(typedoc({
       excludeExternals: true,
@@ -36,45 +22,30 @@ gulp.task('readme', () => {
     }))
 })
 
-gulp.task('tslint', () => {
-  util.log('Linting source code...')
-  const cfg = ts.createProject('tsconfig.json')
-  return cfg.src()
-    .pipe(cached('tslint'))
-    .pipe(tslint({
-      formatter: 'verbose'
-    }))
-    .pipe(tslint.report({
-      summarizeFailureOutput: true
-    }))
+gulp.task('eslint', () => {
+  log('Linting source code...')
+  return gulp
+    .src(['src/**/*.ts'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
 })
 
 gulp.task('typescript', () => {
-  util.log('Transpiling source code...')
+  log('Transpiling source code...')
   const project = ts.createProject('tsconfig.json')
-  return project.src()
-    .pipe(cached('typescript'))
-    .pipe(sourcemaps.init())
-    .pipe(project())
-    .pipe(sourcemaps.write('.', {
-      includeContent: false,
-      sourceRoot: '../src'
-    }))
-    .pipe(gulp.dest('dist'))
+  return project.src().pipe(project()).pipe(gulp.dest('dist'))
 })
 
 gulp.task('build', gulp.series(
-  'clean:dist',
-  'tslint',
+  'eslint',
   'typescript'
 ))
 
 gulp.task('typedoc', gulp.series(
-  'clean:docs',
   'readme'
 ))
 
 gulp.task('default', gulp.series(
-  'clean:dist',
   'typescript'
 ))
