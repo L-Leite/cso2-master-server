c16fd43d8caba48e51fef0bc07766a5bdbe770cb
Luís Leite <luis@leite.xyz>
2020-08-25 18:13:43 +0100

    Prefix database SQL files with a number This allows us to control which tables get initialized first with postgresql's docker image

--- config/init_db.sh
+++ config/init_db.sh
@@ -5,11 +5,11 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 psql postgres postgres -c "CREATE USER cso2_user WITH PASSWORD 'cso2';"
 createdb -U postgres -O cso2_user cso2
 
-psql cso2 cso2_user < $SCRIPTPATH/db/users.sql
-psql cso2 cso2_user < $SCRIPTPATH/db/inventories.sql
-psql cso2 cso2_user < $SCRIPTPATH/db/inventory/buymenues.sql
-psql cso2 cso2_user < $SCRIPTPATH/db/inventory/cosmetics.sql
-psql cso2 cso2_user < $SCRIPTPATH/db/inventory/loadouts.sql
+psql cso2 cso2_user < $SCRIPTPATH/db/00_users.sql
+psql cso2 cso2_user < $SCRIPTPATH/db/01_inventories.sql
+psql cso2 cso2_user < $SCRIPTPATH/db/02_buymenues.sql
+psql cso2 cso2_user < $SCRIPTPATH/db/03_cosmetics.sql
+psql cso2 cso2_user < $SCRIPTPATH/db/04_loadouts.sql
 
 # get inventoryitem oid
 new_oid=$(psql cso2 postgres -c "SELECT oid FROM pg_type WHERE typname = 'inventoryitem';" | sed -n 3p | tr -d ' ')
